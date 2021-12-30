c678224e12466dc7f727e6df6d1161a0e2015481
Justaway97 <97songming@gmail.com>
2021-12-30 11:39:18 +0800

    add front end test

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,15 @@
-from django.urls.conf import path
+from django.shortcuts import render
+from django.urls.conf import path, re_path
 
 from order import views
 
+# pylint: disable=unused-argument
+def index(request, **kwargs):
+    """Returns main page"""
+    return render(request, 'drive/index.html')
 
 urlpatterns = [
     path(r'', views.index),
+    re_path(r'^(?P<path>.*)/$', index),
+    re_path(r'$', index),
 ]
